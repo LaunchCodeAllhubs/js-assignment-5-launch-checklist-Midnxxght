92238e0adbe70748f7f751377fe0b7516cc598c5
Kevin Hernandez <kevinelihern@gmail.com>
2023-08-07 17:53:10 -0500

    updated shuttle requirments

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Write your JavaScript code here!
 
+const { formSubmission } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
 
    let listedPlanets;
@@ -12,5 +14,16 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
+   
+   const form = document.querySelector("form[data-testid='testForm']");
+   form.addEventListener("sumbit",function(event){
+    event.preventDefault();
+    const pilotName = document.getElementById("pilotName").value;
+    const copilotName = document.getElementById("copilotName").value;
+    const fuelLevel = document.getElementById("fuelLevel").value;
+    const cargoLevel = document.getElementById("cargoLevel").value;
+    
+    formSubmission(document, pilotName, copilotName, fuelLevel, cargoLevel);
+   });
    
 });
